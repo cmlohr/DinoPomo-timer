ec4e02348f36122ab4ff608eae04bc2ba50978a8
Colleen <cmlohr@outlook.com>
2020-11-28 22:19:38 -0600

    added buttons, title

--- main.py
+++ main.py
@@ -20,18 +20,24 @@ LONG_BREAK_MIN = 20
 wd = Tk()
 wd.title("DinoPomo")
 wd.config(padx=100, pady=50, bg=BLACK)
-can = Canvas(width=640, height=600, bg=BLACK, highlightthickness=0)
+canvas = Canvas(width=640, height=600, bg=BLACK, highlightthickness=0)
 timer_img = PhotoImage(file="dino.png")
-can.create_image(320, 300, image=timer_img)
-can.create_text(550, 140, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
-can.pack()
+canvas.create_image(320, 300, image=timer_img)
+canvas.create_text(550, 200, text="00:00", fill="white", font=(FONT_NAME, 40, "bold"))
+canvas.grid(column=1, row=1)
 
 
+heading = Label(text="DinoPomo", fg=GREEN, bg=BLACK, font=(FONT_NAME, 30, "bold"))
+heading.grid(column=1, row=0)
 
+start_button = Button(text="START", bg=GREEN, fg=BLACK)
+start_button.grid(column=0, row=3)
 
+reset_button = Button(text="RESET", bg=GREEN, fg=BLACK)
+reset_button.grid(column=2, row=3)
 
-
-
+check = Label(text="✔", bg=BLACK, fg=GREEN, font=(FONT_NAME, 30, "bold"))
+check.grid(column=1, row=3)
 
 
 
