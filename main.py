72a00901f8cd1cd972469c25893d6f9049b61a7f
Colleen <cmlohr@outlook.com>
2020-11-28 21:12:14 -0600

    added canvas structure

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
-PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
+BLACK = "#191919"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-# GUI setup
-wd = Tk()
-wd.title("My Pomodoro")
-wd.minsize(width=400, height=400)
+
+
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
@@ -19,9 +16,15 @@ wd.minsize(width=400, height=400)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
-# ---------------------------- UI SETUP ------------------------------- #
-
-
+# GUI setup
+wd = Tk()
+wd.title("DinoPomo")
+wd.config(padx=100, pady=50, bg=BLACK)
+can = Canvas(width=640, height=600, bg=BLACK, highlightthickness=0)
+timer_img = PhotoImage(file="dino.png")
+can.create_image(320, 300, image=timer_img)
+can.create_text(550, 140, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
+can.pack()
 
 
 
